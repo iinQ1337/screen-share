4d5405a99056b8282d171d5f8fdb0edbdf8f8725
iinQ1337 <106882763+iinQ1337@users.noreply.github.com>
2024-01-09 20:25:41 +0300

    Update Program.cs

--- screen-share/Program.cs
+++ screen-share/Program.cs
@@ -1,69 +1,43 @@
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Fleck;
+using System.Windows.Forms;
+using WebSocketSharp;
 
 class Program
 {
     static void Main()
     {
-        var server = new WebSocketServer("ws://127.0.0.1:3000");
-        server.Start(socket =>
+        const string serverAddress = "ws://0.0.0.0:3000"; // for remote execution replace 0.0.0.0 to ip and port from ngrok (ngrok tcp 3000)
+        using (var ws = new WebSocket(serverAddress))
         {
-            Console.WriteLine($"Connection opend: {socket.ConnectionInfo.ClientIpAddress}");
+            ws.OnOpen += (sender, e) =>
+            {
+                Console.WriteLine("Successfully connected to the WebSocket server");
+            };
 
-            socket.OnClose = () => Console.WriteLine($"Connection closed: {socket.ConnectionInfo.ClientIpAddress}");
+            ws.OnClose += (sender, e) =>
+            {
+                Console.WriteLine($"Connection closed. Code: {e.Code}, Reason: {e.Reason}");
+            };
 
-            _ = Task.Run(() => StartCapture(socket));
+            ws.Connect();
 
-        });
+            while (true)
+            {
+                Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                Graphics gfxScreenshot = Graphics.FromImage(screenshot);
+                gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
 
-        Console.WriteLine("Server started");
-        Console.ReadLine();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    screenshot.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] bitmapData = ms.ToArray();
+                    ws.Send(bitmapData);
+                }
 
-        server.Dispose();
-    }
-
-    private static void StartCapture(IWebSocketConnection socket)
-    {
-        while (socket.IsAvailable)
-        {
-            var screenshot = CaptureScreen();
-            SendImage(socket, screenshot);
-            //Task.Delay(100).Wait(); 
-        }
-    }
-
-    private static Bitmap CaptureScreen()
-    {
-        var screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
-        var screenBmp = new Bitmap(screenSize.Width, screenSize.Height);
-
-        using (var graphics = Graphics.FromImage(screenBmp))
-        {
-            graphics.CopyFromScreen(0, 0, 0, 0, screenSize);
-        }
-
-        return screenBmp;
-    }
-
-    private static void SendImage(IWebSocketConnection socket, Bitmap image)
-    {
-        using (var memoryStream = new MemoryStream())
-        {
-            var encoder = ImageCodecInfo.GetImageEncoders().First(c => c.FormatID == ImageFormat.Jpeg.Guid);
-            var encoderParameters = new EncoderParameters(1);
-            encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 80L); 
-
-            image.Save(memoryStream, encoder, encoderParameters);
-            byte[] imageData = memoryStream.ToArray();
-
-            string base64Image = Convert.ToBase64String(imageData);
-            socket.Send(base64Image);
+                //Thread.Sleep(100);
+            }
         }
     }
 }
